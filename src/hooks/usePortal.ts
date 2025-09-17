631671b44de01d60224eb63824bb01c59f8dc602
kirillzyusko <zyusko.kirik@gmail.com>
2025-09-17 16:59:43 +0200

    fix: changes after self review

--- src/hooks/usePortal.ts
+++ src/hooks/usePortal.ts
@@ -6,7 +6,5 @@ export default function usePortal(hostName: string = "root") {
   return {
     removePortal: (name: string) =>
       dispatch({ type: "REMOVE_PORTAL", hostName, name }),
-    restorePortal: (name: string) =>
-      dispatch({ type: "RESTORE_PORTAL", hostName, name }),
   };
 }
