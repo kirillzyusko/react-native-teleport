a33966ba7e45ae75cba9decccd7e7895065f338d
kirillzyusko <zyusko.kirik@gmail.com>
2025-09-19 12:02:42 +0200

    fix: don't compress image

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -12,7 +12,7 @@ function HomepageHeader() {
   return (
     <header className={clsx(styles.landing, styles.heroBanner)}>
       <div className="container">
-        <img src={require("@site/static/img/demo.png").default} />
+        <img src={require("@site/static/img/gifs/demo.png").default} />
         <Heading as="h1" className={styles.title}>
           {siteConfig.title}
         </Heading>
