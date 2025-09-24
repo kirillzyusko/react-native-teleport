e004bcbfd241c7418a212a3bac14505144206e0b
kirillzyusko <zyusko.kirik@gmail.com>
2025-09-25 00:32:13 +0200

    feat: custom shadow nodes

---
+++ common/cpp/react/renderer/components/reactnativeteleport/RNTPortalViewShadowNode.cpp
@@ -0,0 +1,8 @@
+#include "RNTPortalViewShadowNode.h"
+
+namespace facebook::react
+{
+
+  extern const char PortalViewComponentName[] = "PortalView";
+
+} // namespace facebook::react
---
+++ common/cpp/react/renderer/components/reactnativeteleport/RNTPortalViewShadowNode.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "RNTPortalViewState.h"
+
+#include <react/renderer/components/TeleportViewSpec/EventEmitters.h>
+#include <react/renderer/components/TeleportViewSpec/Props.h>
+#include <react/renderer/components/view/ConcreteViewShadowNode.h>
+#include <jsi/jsi.h>
+
+namespace facebook::react
+{
+
+  JSI_EXPORT extern const char PortalViewComponentName[];
+
+  /*
+   * `ShadowNode` for <PortalView> component.
+   */
+  using PortalViewShadowNode = ConcreteViewShadowNode<
+      PortalViewComponentName,
+      PortalViewProps,
+      PortalViewEventEmitter,
+      PortalViewState>;
+
+} // namespace facebook::react
