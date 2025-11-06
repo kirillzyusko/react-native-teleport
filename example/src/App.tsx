445a9a5d45d029a4864dcf133d9348e11694a676
kirillzyusko <zyusko.kirik@gmail.com>
2025-11-06 10:28:13 +0100

    fix: eslint/tsc

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet } from "react-native";
 import { PortalHost, PortalProvider } from "react-native-teleport";
 import { PortalProvider as GorhomPortalProvider } from "@gorhom/portal";
 import {
