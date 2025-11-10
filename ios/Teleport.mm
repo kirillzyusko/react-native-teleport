ddafc1b7ebfb73ea7cbaeb7e76dc088be4ad8682
kirillzyusko <zyusko.kirik@gmail.com>
2025-11-10 23:09:55 +0100

    fix: objc lang

--- ios/Teleport.mm
+++ ios/Teleport.mm
@@ -13,24 +13,25 @@
 #import <react/renderer/components/TeleportViewSpec/TeleportCommitHook.h>
 
 @implementation Teleport {
-  __weak RCTSurfacePresenter* _surfacePresenter;
+  __weak RCTSurfacePresenter *_surfacePresenter;
   std::shared_ptr<teleport::TeleportCommitHook> commitHook_;
 }
 
 - (void)install
 {
- commitHook_ = std::make_shared<teleport::TeleportCommitHook>(_surfacePresenter.scheduler.uiManager);
+  commitHook_ =
+      std::make_shared<teleport::TeleportCommitHook>(_surfacePresenter.scheduler.uiManager);
 }
 
 - (void)setSurfacePresenter:(id<RCTSurfacePresenterStub>)surfacePresenter
 {
- _surfacePresenter = surfacePresenter;
+  _surfacePresenter = surfacePresenter;
 }
 
 - (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
     (const facebook::react::ObjCTurboModule::InitParams &)params
 {
-    return std::make_shared<facebook::react::NativeTeleportSpecJSI>(params);
+  return std::make_shared<facebook::react::NativeTeleportSpecJSI>(params);
 }
 
 + (NSString *)moduleName
