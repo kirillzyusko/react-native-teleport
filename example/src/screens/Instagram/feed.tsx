a8fb1d715001bcbf9ecf80686cdb14ffc8e19e60
kirillzyusko <zyusko.kirik@gmail.com>
2026-01-02 18:18:13 +0100

    chore: resolve another TODO

--- example/src/screens/Instagram/feed.tsx
+++ example/src/screens/Instagram/feed.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { ScrollView, View, type ScrollEvent } from "react-native";
+import { ScrollView, View, StyleSheet, type ScrollEvent } from "react-native";
 import { CARD_HEIGHT } from "./constants";
 import { posts } from "./posts";
 import Post from "./Post";
@@ -10,12 +10,11 @@ export default function Feed() {
   const onScroll = (e: ScrollEvent) => {
     const { y } = e.nativeEvent.contentOffset;
 
-    // TODO: enable next video when you scrolled 50-60% of CURRENT video
     setIndex(Math.floor(y / (CARD_HEIGHT * 0.75)));
   };
 
   return (
-    <View style={{ flex: 1 }}>
+    <View style={styles.container}>
       <ScrollView onScroll={onScroll}>
         {posts.map((post, i) => (
           <Post active={index === i} key={post.id} post={post} />
@@ -24,3 +23,9 @@ export default function Feed() {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
