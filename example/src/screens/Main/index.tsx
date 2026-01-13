e41759506bfdd6b068f4a07e087b4152a73ef7bd
kirillzyusko <zyusko.kirik@gmail.com>
2026-01-13 13:45:33 +0100

    fix: TS

--- example/src/screens/Main/index.tsx
+++ example/src/screens/Main/index.tsx
@@ -2,13 +2,14 @@ import { useCallback } from "react";
 import { StyleSheet } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
 
-import { ScreenNames, StackNames } from "../../constants/screenNames";
+import { StackNames } from "../../constants/screenNames";
 
 import ExampleLink from "./components/ExampleLink";
 import { examples } from "./constants";
 
 import type { RootStackParamList } from "../../navigation/index";
 import type { NativeStackScreenProps } from "@react-navigation/native-stack";
+import type { ExampleScreen } from "./types";
 
 const styles = StyleSheet.create({
   scrollViewContainer: {
@@ -21,8 +22,10 @@ type Props = NativeStackScreenProps<RootStackParamList>;
 
 const ExampleMain = ({ navigation }: Props) => {
   const onExamplePress = useCallback(
-    (info: ScreenNames) =>
-      navigation.navigate(StackNames.EXAMPLES_STACK, { screen: info }),
+    (info: ExampleScreen) =>
+      navigation.navigate(StackNames.EXAMPLES_STACK, {
+        screen: info,
+      }),
     [navigation],
   );
 
--- example/src/screens/Main/types.ts
+++ example/src/screens/Main/types.ts
@@ -1,8 +1,21 @@
 import type { ScreenNames } from "../../constants/screenNames";
 
+export type ExampleScreen =
+  | ScreenNames.GESTURE_HANDLER_TOUCHABLE
+  | ScreenNames.REACT_NATIVE_TOUCHABLE
+  | ScreenNames.DYNAMIC_CHILDREN
+  | ScreenNames.INSTANT_ROOT
+  | ScreenNames.HOOKS
+  | ScreenNames.FLEXIBLE_STYLES
+  | ScreenNames.PORTAL_BEFORE_HOST
+  | ScreenNames.NAVIGATION_LIFECYCLE
+  | ScreenNames.INSTAGRAM_FEED
+  | ScreenNames.BOTTOM_SHEET
+  | ScreenNames.MESSENGER;
+
 export type Example = {
   testID: string;
   title: string;
-  info: ScreenNames;
+  info: ExampleScreen;
   icons: string;
 };
