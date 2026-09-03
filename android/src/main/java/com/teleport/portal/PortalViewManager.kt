ddffd30788d7e4969f293a4b14883e95823e6eea
Amir Saifutdinov <wuskas47@gmail.com>
2026-09-03 18:34:34 +0300

    style(android): fix ktlint formatting

--- android/src/main/java/com/teleport/portal/PortalViewManager.kt
+++ android/src/main/java/com/teleport/portal/PortalViewManager.kt
@@ -21,8 +21,7 @@ class PortalViewManager :
 
   override fun getDelegate(): ViewManagerDelegate<ReactViewGroup> = delegate
 
-  override fun createTeleportView(context: ThemedReactContext): ReactViewGroup =
-    PortalView(context, ::applyPortalPointerEvents)
+  override fun createTeleportView(context: ThemedReactContext): ReactViewGroup = PortalView(context, ::applyPortalPointerEvents)
 
   override fun onAfterUpdateTransaction(view: ReactViewGroup) {
     super.onAfterUpdateTransaction(view)
